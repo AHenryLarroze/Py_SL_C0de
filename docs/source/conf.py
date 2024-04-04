2164b9756a8a127272c9e72516547fb4847ce179
Totadarck <adrien_henry@orange.fr>
2024-04-04 15:38:47 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import sys, os
-sys.path.insert(0, os.path.commonpath('/slcode'))
+sys.path.insert(0, 'slcode/')
 
 project = 'SL_C0de'
 copyright = '2023, HENRY Adrien'
